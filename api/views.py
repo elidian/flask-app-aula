0ba671b6b39d412b2853422c2f76365daa8efa28
elidian <elidian89@gmail.com>
2020-08-21 16:14:54 -0300

    update wiews METHODS GET POST PUT

--- api/views.py
+++ api/views.py
@@ -10,26 +10,51 @@ students = [
     {'id': 5, 'name': 'Alguem de Alguns', 'age': 20, 'email': 'alal@mail.com'}
 ]
 
+# HOME PAG
 @app.route('/')
 def home():
     return 'My flask api Students'
 
+# BEGIN METHODS GET
 @app.route('/api/students', methods=['GET'])
 def get_students():
     return jsonify({'Students': students})
 
+@app.route('/api/student/<int:student_id>', methods=['GET'])
+def get_student_id(student_id):
+    for student in students:
+        if student['id'] == student_id:
+            return jsonify({'Student': student})
+
+    return jsonify({'Student': None})
+# END METHODS GET
+
+# BEGIN METHODS POST
 @app.route('/api/student', methods=['POST'])
 def create_json_student():
     data = request.get_json()
 
     data['id'] = len(students)+1
     students.append(data)
-    
-    name = data['name']
-    age = data['age']
-    email = data['email']
 
-    return jsonify({'id': data['id'], 'name': name, 'age': age, 'email': email})
+    return jsonify({'id': data['id'], 'name': data['name'], 'age': data['age'], 'email': data['email']})
+# END METHODS POST
+
+# BEGIN METHODS PUT
+@app.route('/api/student', methods=['PUT'])
+def put_student():
+    data = request.get_json()
+
+    for student in students:
+        if student['id'] == data['id']:
+            for inf in data:
+                if data[inf] != student[inf]:
+                    student[inf] = data[inf]
+                    
+            return jsonify({'Student': student})
+    return jsonify({'Student': None})
+# END METHODS PUT
+
 
 """
 books = [
